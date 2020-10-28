34f249436a185bb42067d7c63dddab93eafd492e
4surix <asurix@outlook.fr>
2020-10-28 22:10:43 +0100

    Ajout d'addition entre quark

--- atomion/objets/quark.py
+++ atomion/objets/quark.py
@@ -8,11 +8,11 @@ import random
 
 from .. import objets
 from ..objets import (
-	Proton, Neutron
+    Proton, Neutron
 )
 from .. import exception
 
-from ..utile.typing import Union, Any, Optional
+from ..utile.typing import Union, Any, Optional, List
 
 
 CHARGES_ELECTRIQUES = [
@@ -34,7 +34,7 @@ class Quark:
 
         if charge_electrique not in CHARGES_ELECTRIQUES:
             raise exception.ValeurIncorrecte(
-            	'La charge electrique doit être + 2/3 ou - 1/3.'
+                'La charge electrique doit être + 2/3 ou - 1/3.'
             )
 
         charge_couleur = CHARGES_COULEURS.get(
@@ -42,7 +42,7 @@ class Quark:
         )
         if charge_couleur not in CHARGES_COULEURS.values():
             raise exception.ValeurIncorrecte(
-            	'La charge electrique doit être R, V ou B.'
+                'La charge electrique doit être R, V ou B.'
             )
 
         self.charge_electrique = charge_electrique
@@ -56,6 +56,14 @@ class Quark:
 
     def __eq__(self, obj: Any) -> bool:
         return repr(self) == repr(obj)
+
+    def __add__(self, obj):
+
+        if isinstance(obj, (QUp, QDown)):
+            return BiQuark([self, obj])
+
+        else:
+            raise exception.Incompatible(self, obj)
 
 objets.Quark = Quark
 
@@ -88,6 +96,52 @@ class QDown(Quark):
         )
 
 objets.QDown = QDown
+
+
+class BiQuark:
+
+    __slots__ = ('quarks')
+
+    def __init__(self, quarks:List[Quark]):
+
+        self.quarks = quarks
+
+    def __repr__(self) -> str:
+        return 'BiQuark{%s}' % self.quarks
+
+    def __eq__(self, obj: Any) -> bool:
+        return repr(self) == repr(obj)
+
+    def __add__(self, 
+            obj:Union[QUp, QDown]
+        ) -> Union[Neutron, Proton]:
+
+        if isinstance(obj, (QUp, QDown)):
+
+            value = sum(
+                (quark.charge_electrique for quark in self.quarks),
+                obj.charge_electrique
+            )
+
+            if value == 0:
+                hadron = Neutron()
+
+            elif value == 1:
+                hadron = Proton()
+
+            else:
+                raise exception.Instable(self)
+
+            if 14 - sum(
+                (quark.charge_couleur **2 for quark in self.quarks),
+                obj.charge_couleur **2
+            ) != 0:
+                raise exception.Instable(hadron)
+
+            return hadron
+
+        else:
+            raise exception.Incompatible(self, obj)
 
 
 def MAJ_TYPE():
