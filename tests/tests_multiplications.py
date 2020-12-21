5598200fa8d848576240a6da86f3b40da7018aed
4surix <asurix@outlook.fr>
2020-12-21 19:22:52 +0100

    Ajout tests multiplication

--- tests/tests_multiplications.py
+++ tests/tests_multiplications.py
@@ -11,7 +11,16 @@ from atomion import *
 class TestMultiplication(unittest.TestCase):
 
     def test_atome_int(self):
-        self.assertEqual(Atome('O') * 2, Molecule('O2'))
+        self.assertEqual(2 * Atome('O') * 2, Molecule('O4'))
+
+    def test_neutron_int(self):
+        self.assertEqual(2 * Neutron() * 2, Neutron(4))
+
+    def test_proton_int(self):
+        self.assertEqual(2 * Proton() * 2, Proton(4))
+
+    def test_electron_int(self):
+        self.assertEqual(2 * Electron() * 2, Electron(4))
 
 
 if __name__ == '__main__':
